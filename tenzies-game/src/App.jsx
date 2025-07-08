23a354b44f7c50c2fe0cf3556cb4f95314700b04
motinxyz <motinsghs@gmail.com>
2025-07-09 01:15:32 +0600

    Tenzies Game Completed

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,4 +1,5 @@
 import MainSection from "./components/MainSection";
+import "./styles/App.css";
 
 export default function App(){
     return(
--- tenzies-game/src/components/Die.jsx
+++ tenzies-game/src/components/Die.jsx
@@ -1,7 +1,15 @@
-import "../css/Die.css";
+import "../styles/Die.css";
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
   return (
-    <button className="die">{props.value}</button>
+    <button 
+    style={styles} 
+    onClick={() => props.hold(props.id)} 
+    className="die">
+      {props.value}
+    </button>
   );
 }
